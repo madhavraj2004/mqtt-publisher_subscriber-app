024cb7360b2cc93d66e1a9aa7dbe4fd0754170c7
madhavraj2004 <raj.madhav2004@gmail.com>
2024-12-23 19:50:19 +0530

    sucessfulyy publishing to and receiving...

--- app/src/main/java/com/example/task/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/task/ui/dashboard/DashboardFragment.java
@@ -1,16 +1,27 @@
 package com.example.task.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.task.databinding.FragmentDashboardBinding;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import timber.log.Timber;
 
 public class DashboardFragment extends Fragment {
 
@@ -26,6 +37,21 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        OkHttpClient client = new OkHttpClient();
+        String url = "http://52.250.54.24:3500/api/node/";
+        Request getRequest=new Request.Builder().url(url).build();
+        client.newCall(getRequest).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                System.out.println(response.body().string());
+            }
+        });
+
         return root;
     }
 
